4caffe4deedbe75055b24292ced06b184d37e566
{qaw302} <{osshun21@gmail.com}>
2024-01-29 20:26:30 +0900

    feat : polymorphism 실습 1

--- src/main/java/com/nhnacademy/edu/springframework/Main.java
+++ src/main/java/com/nhnacademy/edu/springframework/Main.java
@@ -2,6 +2,17 @@ package com.nhnacademy.edu.springframework;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Main main = new Main();
+        User user = new User("NHN@ACADEMY.com", "010-1111-2222");
+        String message = "polymorphism 실습 1";
+        main.sendSmsMessage(user, message);
+    }
+
+    private void sendSmsMessage(User user, String message) {
+        System.out.println("[SMS] message sent to " +user.getPhoneNumber() + " : " + message);
+    }
+
+    private void sendEmailMessage(User user, String message) {
+        System.out.println("[Email] message sent to " +user.getPhoneNumber() + ":" + message);
     }
 }
